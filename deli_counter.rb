71aff223f5d53543d04b0e3e37f1165d11384dbd
Nicholas Dover <dovern42@gmail.com>
2019-02-10 02:08:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
+require 'pry'
 # Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    array.each_with_index do |name, index| string.concat(" #{index+1}. #{name}")}
+    end
+    string
+  end
+end
